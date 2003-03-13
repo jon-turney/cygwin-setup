554b7090222595d22ef2ee9327c453f48f7cb2c1
cvs2svn <>
2003-03-13 12:30:15 +0000

    This commit was manufactured by cvs2svn to create tag 'ZLIB_1_1_4'.
    Sprout from IMPORT 2003-03-13 12:30:14 UTC Max Bowsher <maxb1@ukf.net> 'Import zlib-1.1.4'
    Delete:
        ChangeLog
        Makefile
        README
        error.c
        memory.c
        resource.h
        setup.c
        setup.dsp
        setup.dsw
        setup.h
        setupres.rc
        strarry.c
        strarry.h
        zlib.dsp

--- strarry.c
+++
@@ -1,28 +0,0 @@
-/* strarry.c: implementation of the strarry struct routines. */  
-#include <malloc.h>
-#include "setup.h"
-#include "strarry.h"
-void 
-sa_init (SA * array) 
-{
-  array->count = 0;
-  array->array = NULL;
-}
-void 
-sa_add (SA * array, const char *str) 
-{
-  array->array = array->count 
-    ? xrealloc (array->array, sizeof (char *) * (array->count + 1)) 
-    : xmalloc (sizeof (char *));
-  array->array[array->count++] = xstrdup (str);
-}
-void 
-sa_cleanup (SA * array) 
-{
-  size_t n = array->count;
-  while (n--)
-    xfree (array->array[n]);
-  xfree (array->array);
-}
-
-
--- strarry.h
+++
@@ -1,13 +0,0 @@
-/* strarry.h: strarry struct */  
-  
-#include <stddef.h>
-typedef struct strarry 
-{
-  char **array;
-   size_t count;
- }
- SA;
-void sa_init (SA *);		/* Initialize the struct. */
-void sa_add (SA *, const char *);	/* Add a string to the array. */
-void sa_cleanup (SA *);	/* Deallocate all of the memory. */
-
