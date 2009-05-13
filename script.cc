b7940eb57d857e9409103366238f6c965d88914b
Corinna Vinschen <corinna@vinschen.de>
2009-05-13 11:44:32 +0000

    * script.cc (Script::run): Start bash with --norc --noprofile options.

--- script.cc
+++ script.cc
@@ -223,7 +223,7 @@ Script::run() const
   OutputLog file_out = std::string (mktemp (tmp_pat));
   if (sh.size() && stricmp (extension(), ".sh") == 0)
     {
-      log(LOG_PLAIN) << "running: " << sh << " -c " << scriptName << endLog;
+      log(LOG_PLAIN) << "running: " << sh << " --norc --noprofile -c " << scriptName << endLog;
       retval = ::run (sh.c_str(), "-c", scriptName.c_str(), file_out);
     }
   else if (cmd && stricmp (extension(), ".bat") == 0)
