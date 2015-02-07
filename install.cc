4782666e90fd62234037a0eb9db549c37e045db6
Achim Gratz <Stromeko@NexGo.DE>
2015-02-07 21:18:05 +0000

    * install.cc (preremoveOne): Allow dash scripts and ".cmd" as an 	additional extension for Windows batch files.

--- install.cc
+++ install.cc
@@ -159,8 +159,10 @@ Installer::preremoveOne (packagemeta & pkg)
   Progress.SetText1 ("Running preremove script...");
   Progress.SetText2 (pkg.name.c_str());
   Log (LOG_PLAIN) << "Running preremove script for  " << pkg.name << endLog;
-  try_run_script ("/etc/preremove/", pkg.name, ".sh");
-  try_run_script ("/etc/preremove/", pkg.name, ".bat");
+  const unsigned numexts = 4;
+  const char* exts[numexts] = { ".dash", ".sh", ".bat", ".cmd" };
+  for (unsigned i = 0; i < numexts; i++)
+    try_run_script ("/etc/preremove/", pkg.name, exts[i]);
 }
 
 void
