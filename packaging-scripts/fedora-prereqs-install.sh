83e99d0055812a9c3e5f1fe43675cd2e45078c4f
Jon Turney <jon.turney@dronecode.org.uk>
2016-06-21 17:29:41 +0100

    Optionally sign setup using osslsigntool

--- packaging-scripts/fedora-prereqs-install.sh
+++ packaging-scripts/fedora-prereqs-install.sh
@@ -3,6 +3,7 @@
 sudo dnf -y install automake bison diffutils flex libtool make upx
 sudo dnf -y install mingw32-gcc-c++ mingw32-zlib-static mingw32-libgcrypt-static mingw32-bzip2-static mingw32-xz-libs-static mingw32-winpthreads-static
 sudo dnf -y install mingw64-gcc-c++ mingw64-zlib-static mingw64-libgcrypt-static mingw64-bzip2-static mingw64-xz-libs-static mingw64-winpthreads-static
+sudo dnf -y install openssl-pkcs11 osslsigncode
 sudo dnf -y install 'dnf-command(copr)'
 sudo dnf -y copr enable jturney/mingw-libsolv
 sudo dnf -y install mingw32-libsolv-static mingw64-libsolv-static
