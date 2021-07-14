jon-turney/cygwin-setup

commit f34a20e7adaead364fd29c8f00393fdfcfba359b

Move package action strings to string table resource
Move package action strings to string table resource.
Switch ListView control to wide-char.

ListView.cc
@@ -14,6 +14,7 @@
 #include "ListView.h"
 #include "LogSingleton.h"
 #include "resource.h"
+#include "String++.h"
 
 #include <commctrl.h>
 
@@ -95,6 +96,9 @@ ListView::init(HWND parent, int id, HeaderList headers)
   SendMessage(hWndTip, TTM_SETDELAYTIME, TTDT_AUTOPOP, (LPARAM) MAKELONG (30000, 0));
   // match tip width used elsewhere
   SendMessage(hWndTip, TTM_SETMAXTIPWIDTH, 0, 450);
+
+  // switch to using wide-char WM_NOTIFY messages
+  ListView_SetUnicodeFormat(hWndListView, TRUE);
 }
 
 void
@@ -256,29 +260,30 @@ ListView::setContents(ListViewContents *_contents, bool tree)
   RedrawWindow(hWndListView, NULL, NULL, RDW_ERASE | RDW_FRAME | RDW_INVALIDATE | RDW_ALLCHILDREN);
 }
 
-// Helper class: The char * pointer we hand back needs to remain valid for some
-// time after OnNotify returns, when the std::string we have retrieved has gone
-// out of scope, so a static instance of this class maintains a local cache.
-class StringCache
+// Helper class: The pointer we hand back needs to remain valid for some time
+// after OnNotify returns, when the string object we have retrieved has gone out
+// of scope, so a static instance of this class maintains a local cache.
+template <class T> class StringCache
 {
+  typedef typename T::traits_type::char_type char_type;
 public:
   StringCache() : cache(NULL), cache_size(0) { }
-  StringCache & operator = (const std::string & s)
+  StringCache & operator = (const T & s)
   {
     if ((s.length() + 1) > cache_size)
       {
         cache_size = s.length() + 1;
-        cache = (char *)realloc(cache, cache_size);
+        cache = (char_type *)realloc(cache, cache_size * sizeof(char_type));
       }
-    strcpy(cache, s.c_str());
+    memcpy(cache, s.c_str(), cache_size * sizeof(char_type));
     return *this;
   }
-  operator char *() const
+  operator char_type *() const
   {
     return cache;
   }
 private:
-  char *cache;
+  char_type *cache;
   size_t cache_size;
 };
 
@@ -291,9 +296,9 @@ ListView::OnNotify (NMHDR *pNmHdr, LRESULT *pResult)
 
   switch (pNmHdr->code)
   {
-  case LVN_GETDISPINFO:
+  case LVN_GETDISPINFOW:
     {
-      NMLVDISPINFO *pNmLvDispInfo = (NMLVDISPINFO *)pNmHdr;
+      NMLVDISPINFOW *pNmLvDispInfo = (NMLVDISPINFOW *)pNmHdr;
 #if DEBUG
       Log (LOG_BABBLE) << "LVN_GETDISPINFO " << pNmLvDispInfo->item.iItem << endLog;
 #endif
@@ -302,7 +307,7 @@ ListView::OnNotify (NMHDR *pNmHdr, LRESULT *pResult)
           int iRow = pNmLvDispInfo->item.iItem;
           int iCol = pNmLvDispInfo->item.iSubItem;
 
-          static StringCache s;
+          static StringCache<std::wstring> s;
           s = (*contents)[iRow]->get_text(iCol);
           pNmLvDispInfo->item.pszText = s;
 
@@ -429,7 +434,7 @@ ListView::OnNotify (NMHDR *pNmHdr, LRESULT *pResult)
 
               case ListView::ControlType::checkbox:
                 {
-                  // get the subitem text
+                  // get the subitem text (as ASCII)
                   char buf[3];
                   ListView_GetItemText(hWndListView, iRow, iCol, buf, _countof(buf));
 
@@ -592,7 +597,7 @@ ListView::OnNotify (NMHDR *pNmHdr, LRESULT *pResult)
 #endif
 
       // get the tooltip text for that item/subitem
-      static StringCache tooltip;
+      static StringCache<std::string> tooltip;
       tooltip = "";
       if (contents)
         tooltip = (*contents)[iRow]->get_tooltip(iCol);
@@ -631,7 +636,7 @@ ListView::popup_menu(int iRow, int iCol, POINT p)
   // construct menu
   HMENU hMenu = CreatePopupMenu();
 
-  MENUITEMINFO mii;
+  MENUITEMINFOW mii;
   memset(&mii, 0, sizeof(mii));
   mii.cbSize = sizeof(mii);
   mii.fMask = MIIM_FTYPE | MIIM_STATE | MIIM_STRING | MIIM_ID;
@@ -644,12 +649,12 @@ ListView::popup_menu(int iRow, int iCol, POINT p)
   for (i = al->list.begin (); i != al->list.end (); ++i, ++j)
     {
       BOOL res;
-      mii.dwTypeData = (char *)i->name.c_str();
+      mii.dwTypeData = const_cast <wchar_t *> (i->name.c_str());
       mii.fState = (i->selected ? MFS_CHECKED : MFS_UNCHECKED |
                     i->enabled ? MFS_ENABLED : MFS_DISABLED);
       mii.wID = j;
 
-      res = InsertMenuItem(hMenu, -1, TRUE, &mii);
+      res = InsertMenuItemW(hMenu, -1, TRUE, &mii);
       if (!res) Log (LOG_BABBLE) << "InsertMenuItem failed " << endLog;
     }
 

ActionList.h
@@ -16,6 +16,7 @@
 
 #include <string>
 #include <vector>
+#include "win32.h"
 
 // ---------------------------------------------------------------------------
 // interface to class ActionList
@@ -26,14 +27,14 @@
 class Action
 {
  public:
-  Action(const std::string &_name, int _id, bool _selected, bool _enabled) :
+  Action(const std::wstring &_name, int _id, bool _selected, bool _enabled) :
     name(_name),
     id(_id),
     selected(_selected),
     enabled(_enabled)
   { };
 
-  std::string name;
+  std::wstring name;
   int id;
   bool selected;
   bool enabled;
@@ -44,10 +45,15 @@ typedef std::vector<Action> Actions;
 class ActionList
 {
  public:
-  void add(const std::string &name, int id, bool selected, bool enabled)
+  void add(const std::wstring &name, int id, bool selected, bool enabled)
   {
     Action act(name, id, selected, enabled);
     list.push_back(act);
+  };
+  void add(unsigned int name_res, int id, bool selected, bool enabled)
+  {
+    const std::wstring name = LoadStringW(name_res);
+    add(name, id, selected, enabled);
   };
   Actions list;
 };

ListView.h
@@ -31,7 +31,7 @@ class ListViewLine
   enum class State { collapsed, expanded, nothing=-1 };
 
   virtual ~ListViewLine() {};
-  virtual const std::string get_text(int col) const = 0;
+  virtual const std::wstring get_text(int col) const = 0;
   virtual State get_state() const = 0;
   virtual const std::string get_tooltip(int col) const = 0;
   virtual int get_indent() const = 0;

PickCategoryLine.cc
@@ -18,9 +18,10 @@
 #include "PickView.h"
 #include "window.h"
 #include "package_meta.h"
+#include "resource.h"
 #include <sstream>
 
-const std::string
+const std::wstring
 PickCategoryLine::get_text (int col_num) const
 {
   if (col_num == pkgname_col)
@@ -29,13 +30,13 @@ PickCategoryLine::get_text (int col_num) const
       s << cat_tree->category().first;
       if (pkgcount)
         s << " (" << pkgcount << ")";
-      return s.str();
+      return string_to_wstring(s.str());
     }
   else if (col_num == new_col)
     {
-      return packagemeta::action_caption (cat_tree->action());
+      return LoadStringW(packagemeta::action_caption (cat_tree->action()));
     }
-  return "";
+  return L"";
 }
 
 int
@@ -68,11 +69,11 @@ PickCategoryLine::get_actions(int col) const
   ActionList *al = new ActionList();
   packagemeta::_actions current_default = cat_tree->action();
 
-  al->add("Default", (int)packagemeta::NoChange_action, (current_default == packagemeta::NoChange_action), TRUE);
-  al->add("Install", (int)packagemeta::Install_action, (current_default == packagemeta::Install_action), TRUE);
-  al->add(packagedb::task == PackageDB_Install ? "Reinstall" : "Retrieve",
+  al->add(IDS_ACTION_DEFAULT, (int)packagemeta::NoChange_action, (current_default == packagemeta::NoChange_action), TRUE);
+  al->add(IDS_ACTION_INSTALL, (int)packagemeta::Install_action, (current_default == packagemeta::Install_action), TRUE);
+  al->add(packagedb::task == PackageDB_Install ? IDS_ACTION_REINSTALL : IDS_ACTION_RETRIEVE,
           (int)packagemeta::Reinstall_action, (current_default == packagemeta::Reinstall_action), TRUE);
-  al->add("Uninstall", (int)packagemeta::Uninstall_action, (current_default == packagemeta::Uninstall_action), TRUE);
+  al->add(IDS_ACTION_UNINSTALL, (int)packagemeta::Uninstall_action, (current_default == packagemeta::Uninstall_action), TRUE);
 
   return al;
 }

PickCategoryLine.h
@@ -34,7 +34,7 @@ public:
   {
   }
 
-  const std::string get_text(int col) const;
+  const std::wstring get_text(int col) const;
   State get_state() const;
   const std::string get_tooltip(int col) const;
   int get_indent() const;

PickPackageLine.cc
@@ -17,16 +17,16 @@
 #include "PickView.h"
 #include "package_db.h"
 
-const std::string
+const std::wstring
 PickPackageLine::get_text(int col_num) const
 {
   if (col_num == pkgname_col)
     {
-      return pkg.name;
+      return string_to_wstring(pkg.name);
     }
   else if (col_num == current_col)
     {
-      return pkg.installed.Canonical_version ();
+      return string_to_wstring(pkg.installed.Canonical_version());
     }
   else if (col_num == new_col)
     {
@@ -44,11 +44,11 @@ PickPackageLine::get_text(int col_num) const
       else
         srctick = "no";
 
-      return srctick;
+      return string_to_wstring(srctick);
     }
   else if (col_num == cat_col)
     {
-      return pkg.getReadableCategoryList();
+      return string_to_wstring(pkg.getReadableCategoryList());
     }
   else if (col_num == size_col)
     {
@@ -75,14 +75,14 @@ PickPackageLine::get_text(int col_num) const
       /* If size still 0, size must be unknown.  */
       std::string size = (sz == 0) ? "?" : format_1000s((sz+1023)/1024) + "k";
 
-      return size;
+      return string_to_wstring(size);
     }
   else if (col_num == pkg_col)
     {
-      return pkg.SDesc();
+      return string_to_wstring(pkg.SDesc());
     }
 
-  return "unknown";
+  return L"unknown";
 }
 
 const std::string

PickPackageLine.h
@@ -30,7 +30,7 @@ public:
     indent (aindent)
   {
   };
-  const std::string get_text(int col) const;
+  const std::wstring get_text(int col) const;
   State get_state() const { return State::nothing; }
   const std::string get_tooltip(int col) const;
   int get_indent() const;

package_meta.cc
@@ -52,22 +52,22 @@ bool hasManualSelections = 0;
 /*****************/
 
 /* Return an appropriate category caption given the action */
-char const *
+unsigned int
 packagemeta::action_caption (_actions _value)
 {
   switch (_value)
     {
     case NoChange_action:
-      return "Default";
+      return IDS_ACTION_DEFAULT;
     case Install_action:
-      return "Install";
+      return IDS_ACTION_INSTALL;
     case Reinstall_action:
-      return "Reinstall";
+      return IDS_ACTION_REINSTALL;
     case Uninstall_action:
-      return "Uninstall";
+      return IDS_ACTION_UNINSTALL;
     }
 
-  return "Unknown";
+  return IDS_ACTION_UNKNOWN;
 }
 
 packagemeta::packagemeta (packagemeta const &rhs) :
@@ -440,26 +440,26 @@ packagemeta::LDesc () const
 };
 
 /* Return an appropriate caption given the current action. */
-std::string
+std::wstring
 packagemeta::action_caption () const
 {
   switch (_action)
     {
     case Uninstall_action:
-      return "Uninstall";
+      return LoadStringW(IDS_ACTION_UNINSTALL);
     case NoChange_action:
       if (!desired)
-        return "Skip";
+        return LoadStringW(IDS_ACTION_SKIP);
       if (desired.sourcePackage() && srcpicked())
         /* FIXME: Redo source should come up if the tarball is already present locally */
-        return "Source";
-      return "Keep";
+        return LoadStringW(IDS_ACTION_SOURCE);
+      return LoadStringW(IDS_ACTION_KEEP);
     case Reinstall_action:
-      return packagedb::task == PackageDB_Install ? "Reinstall" : "Retrieve";
+      return LoadStringW(packagedb::task == PackageDB_Install ? IDS_ACTION_REINSTALL : IDS_ACTION_RETRIEVE);
     case Install_action:
-      return desired.Canonical_version ();
+      return string_to_wstring(desired.Canonical_version());
     }
-  return "Unknown";
+  return LoadStringW(IDS_ACTION_UNKNOWN);
 }
 
 void
@@ -510,23 +510,23 @@ packagemeta::list_actions(trusts const trust)
   // build the list of possible actions
   ActionList *al = new ActionList();
 
-  al->add("Uninstall", (int)Uninstall_action, (_action == Uninstall_action), bool(installed));
-  al->add("Skip", (int)NoChange_action, (_action == NoChange_action) && !installed, !installed);
+  al->add(IDS_ACTION_UNINSTALL, (int)Uninstall_action, (_action == Uninstall_action), bool(installed));
+  al->add(IDS_ACTION_SKIP, (int)NoChange_action, (_action == NoChange_action) && !installed, !installed);
 
   std::set<packageversion>::iterator i;
   for (i = versions.begin (); i != versions.end (); ++i)
     {
       if (*i == installed)
         {
-          al->add("Keep", (int)NoChange_action, (_action == NoChange_action), TRUE);
-          al->add(packagedb::task == PackageDB_Install ? "Reinstall" : "Retrieve",
+          al->add(IDS_ACTION_KEEP, (int)NoChange_action, (_action == NoChange_action), TRUE);
+          al->add(packagedb::task == PackageDB_Install ? IDS_ACTION_REINSTALL : IDS_ACTION_RETRIEVE,
                   (int)Reinstall_action, (_action == Reinstall_action), TRUE);
         }
       else
         {
-          std::string label = i->Canonical_version().c_str();
+          std::wstring label = string_to_wstring(i->Canonical_version());
           if (packagedb::solver.is_test_package(*i))
-            label += " (Test)";
+            label += L" (Test)";
           al->add(label,
                   -std::distance(versions.begin (), i),
                   (_action == Install_action) && (*i == desired),
@@ -728,11 +728,10 @@ packagemeta::logSelectionStatus() const
   packagemeta const & pkg = *this;
   const char *trust = ((pkg.desired == pkg.curr) ? "curr"
                : (pkg.desired == pkg.exp) ? "test" : "unknown");
-  std::string action = pkg.action_caption ();
   const std::string installed =
    pkg.installed ? pkg.installed.Canonical_version () : "none";
 
-  Log (LOG_BABBLE) << "[" << pkg.name << "] action=" << action << " trust=" << trust << " installed=" << installed << " src?=" << (pkg.desired && srcpicked() ? "yes" : "no") << endLog;
+  Log (LOG_BABBLE) << "[" << pkg.name << "] action=" << _action << " trust=" << trust << " installed=" << installed << " src?=" << (pkg.desired && srcpicked() ? "yes" : "no") << endLog;
   if (pkg.categories.size ())
     Log (LOG_BABBLE) << "     categories=" << for_each(pkg.categories.begin(), pkg.categories.end(), StringConcatenator(", ")).result << endLog;
 #if 0

commit 77e4675a9603cdd5757152a744f9ff7e53bdbafc

Fix a crash in ListView StringCache
Fix a sporadic crash in chooser introduced in f34a20e7.

Limit the mempcy() to fill StringCache to the size of the string we are
putting in the cache, not the the size of the cache (which will fault if
it happens to over-run into a inaccessible page).

ListView.cc
@@ -275,7 +275,7 @@ public:
         cache_size = s.length() + 1;
         cache = (char_type *)realloc(cache, cache_size * sizeof(char_type));
       }
-    memcpy(cache, s.c_str(), cache_size * sizeof(char_type));
+    memcpy(cache, s.c_str(), (s.length() + 1) * sizeof(char_type));
     return *this;
   }
   operator char_type *() const