jon-turney/cygwin-setup

commit b3cc8ab6692d0ff958a999fe690424fb90b79a8b

Move installation progress strings to string table resource

ini.cc
@@ -71,7 +71,7 @@ public:
     {
       Progress.SetText2 ("");
       Progress.SetText3 ("");
-      Progress.SetText4 ("Progress:");
+      Progress.SetText4 (IDS_PROGRESS_PARSING);
 
       yyerror_count = 0;
       yyerror_messages.clear ();
@@ -98,7 +98,7 @@ public:
     }
   virtual void iniName (const std::string& name)
     {
-      Progress.SetText1 ("Parsing...");
+      Progress.SetText1 (IDS_PROGRESS_PARSING);
       Progress.SetText2 (name.c_str ());
       Progress.SetText3 ("");
       filename = name;
@@ -134,7 +134,7 @@ public:
     }
   virtual ~ GuiParseFeedback ()
     {
-      Progress.SetText4 ("Package:");
+      Progress.SetText4 (IDS_PROGRESS_PACKAGE);
     }
 private:
   unsigned int lastpct;

download.cc
@@ -278,7 +278,7 @@ do_download_thread (HINSTANCE h, HWND owner)
   total_download_bytes_sofar = 0;
   download_failures.clear ();
 
-  Progress.SetText1 ("Checking for packages to download...");
+  Progress.SetText1 (IDS_PROGRESS_CHECKING);
   Progress.SetText2 ("");
   Progress.SetText3 ("");
 

geturl.cc
@@ -62,10 +62,10 @@ init_dialog (const std::string &url, int length)
 
   std::string::size_type divide = url.find_last_of('/');
   max_bytes = length;
-  Progress.SetText1("Downloading...");
+  Progress.SetText1(IDS_PROGRESS_DOWNLOADING);
   Progress.SetText2((url.substr(divide + 1) + " from "
                      + url.substr(0, divide)).c_str());
-  Progress.SetText3("Connecting...");
+  Progress.SetText3(IDS_PROGRESS_CONNECTING);
   Progress.SetBar1(0);
   start_tics = GetTickCount ();
 }

install.cc
@@ -148,7 +148,7 @@ static int num_installs, num_uninstalls;
 void
 Installer::preremoveOne (packagemeta & pkg)
 {
-  Progress.SetText1 ("Running preremove script...");
+  Progress.SetText1 (IDS_PROGRESS_PREREMOVE);
   Progress.SetText2 (pkg.name.c_str());
   Log (LOG_BABBLE) << "Running preremove script for " << pkg.name << endLog;
   const unsigned numexts = 4;
@@ -163,7 +163,7 @@ Installer::uninstallOne (packagemeta & pkg)
   if (!pkg.installed)
     return;
 
-  Progress.SetText1 ("Uninstalling...");
+  Progress.SetText1 (IDS_PROGRESS_UNINSTALL);
   Progress.SetText2 (pkg.name.c_str());
   Log (LOG_PLAIN) << "Uninstalling " << pkg.name << endLog;
 
@@ -427,7 +427,7 @@ Installer::installOne (packagemeta &pkgm, const packageversion &ver,
 {
   if (!source.Canonical())
     return;
-  Progress.SetText1 ("Installing");
+  Progress.SetText1 (IDS_PROGRESS_INSTALL);
   Progress.SetText2 ((pkgm.name + "-" + ver.Canonical_version()).c_str());
 
   io_stream *pkgfile = NULL;
@@ -818,7 +818,7 @@ do_install_thread (HINSTANCE h, HWND owner)
   const SolverTransactionList &t = db.solution.transactions();
 
   /* Calculate the amount of data to md5sum */
-  Progress.SetText1("Calculating...");
+  Progress.SetText1(IDS_PROGRESS_CALCULATING);
   long long int md5sum_total_bytes = 0;
   for (SolverTransactionList::const_iterator i = t.begin (); i != t.end (); ++i)
   {

package_source.cc
@@ -25,6 +25,7 @@
 #include "Exception.h"
 #include "filemanip.h"
 #include "io_stream.h"
+#include "resource.h"
 
 extern ThreeBarProgressPage Progress;
 
@@ -113,7 +114,7 @@ packagesource::check_sha512 (const std::string fullname) const
   Log (LOG_BABBLE) << "Checking SHA512 for " << fullname << endLog;
 
   Progress.SetText1 (("Checking SHA512 for " + shortname).c_str ());
-  Progress.SetText4 ("Progress:");
+  Progress.SetText4 (IDS_PROGRESS_PROGRESS);
   Progress.SetBar1 (0);
 
   unsigned char buffer[64 * 1024];
@@ -162,7 +163,7 @@ packagesource::check_md5 (const std::string fullname) const
   Log (LOG_BABBLE) << "Checking MD5 for " << fullname << endLog;
 
   Progress.SetText1 (("Checking MD5 for " + shortname).c_str ());
-  Progress.SetText4 ("Progress:");
+  Progress.SetText4 (IDS_PROGRESS_PROGRESS);
   Progress.SetBar1 (0);
 
   unsigned char buffer[64 * 1024];

postinstall.cc
@@ -145,7 +145,7 @@ private:
 static std::string
 do_postinstall_thread (HINSTANCE h, HWND owner)
 {
-  Progress.SetText1 ("Running...");
+  Progress.SetText1 (IDS_PROGRESS_POSTINSTALL);
   Progress.SetText2 ("");
   Progress.SetText3 ("");
   Progress.SetBar1 (0, 1);

prereq.cc
@@ -163,7 +163,7 @@ PrereqChecker::isMet ()
 {
   packagedb db;
 
-  Progress.SetText1 ("Solving dependencies...");
+  Progress.SetText1 (IDS_PROGRESS_SOLVING);
   Progress.SetText2 ("");
   Progress.SetText3 ("");
 

res.rc
@@ -607,4 +607,16 @@ BEGIN
     IDS_ELEVATED       "Hand installation over to elevated child process."
     IDS_INSTALLEDB_VERSION "Unknown INSTALLED.DB version"
     IDS_NO_MIRROR_LST  "Could not download mirror sites list"
+    IDS_PROGRESS_PROGRESS "Progress:"
+    IDS_PROGRESS_PACKAGE "Package:"
+    IDS_PROGRESS_CONNECTING "Connecting..."
+    IDS_PROGRESS_CHECKING "Checking for packages to download..."
+    IDS_PROGRESS_DOWNLOADING "Downloading..."
+    IDS_PROGRESS_PARSING "Parsing..."
+    IDS_PROGRESS_PREREMOVE "Running preremove script..."
+    IDS_PROGRESS_UNINSTALL "Uninstalling..."
+    IDS_PROGRESS_INSTALL "Installing..."
+    IDS_PROGRESS_CALCULATING "Calculating..."
+    IDS_PROGRESS_POSTINSTALL "Running..."
+    IDS_PROGRESS_SOLVING "Solving dependencies..."
 END

commit 489d9c53cc7c795ff5f6b8016b8613e321e5845c

Reset GUI feedback state after ini parsing
If we're in --local-install mode, we're shortly going to disappear into
packagemeta::ScanDownloadedFiles() which (currently) gives no feedback.

Don't make it look like we're still in the middle of parsing ini files
while we do that.

Also fix a typo introduced in b3cc8ab6.

ini.cc
@@ -69,9 +69,10 @@ class GuiParseFeedback : public IniParseFeedback
 public:
   GuiParseFeedback () : lastpct (0)
     {
+      Progress.SetText1 (IDS_PROGRESS_PARSING);
       Progress.SetText2 ("");
       Progress.SetText3 ("");
-      Progress.SetText4 (IDS_PROGRESS_PARSING);
+      Progress.SetText4 (IDS_PROGRESS_PROGRESS);
 
       yyerror_count = 0;
       yyerror_messages.clear ();
@@ -98,7 +99,6 @@ public:
     }
   virtual void iniName (const std::string& name)
     {
-      Progress.SetText1 (IDS_PROGRESS_PARSING);
       Progress.SetText2 (name.c_str ());
       Progress.SetText3 ("");
       filename = name;
@@ -134,7 +134,10 @@ public:
     }
   virtual ~ GuiParseFeedback ()
     {
+      Progress.SetText2 ("");
+      Progress.SetText3 ("");
       Progress.SetText4 (IDS_PROGRESS_PACKAGE);
+      Progress.SetBar1 (0);
     }
 private:
   unsigned int lastpct;