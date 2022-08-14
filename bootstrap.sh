82e62d6d8f9dcb9ba2a34da0e4437b29402951f7
Jon Turney <jon.turney@dronecode.org.uk>
2022-08-14 17:00:29 +0100

    Update bootstrap.sh
    Prefer x64 toolchain, if available
    Don't use x86 MinGW.org toolchain (no longer distributed in cygwin since 2016)

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,12 +28,10 @@ fi
 
 if [[ "$1" =~ "--host=" ]]; then
 	host="${1#--host=}"
-elif hash i686-w64-mingw32-g++ 2> /dev/null; then
-	host="i686-w64-mingw32"
-elif hash i686-pc-mingw32-g++ 2> /dev/null; then
-	host="i686-pc-mingw32"
 elif hash x86_64-w64-mingw32-g++ 2> /dev/null; then
 	host="x86_64-w64-mingw32"
+elif hash i686-w64-mingw32-g++ 2> /dev/null; then
+	host="i686-w64-mingw32"
 else
 	echo "mingw32 or mingw64 target g++ required for building setup"
 	exit 1
