jon-turney/cygwin-setup

commit b0bd16950669290e488dc382051f7f28f2278eef

Fix '-t -P package-with-only-a-test-version'
Fix how 'install any version' operates in that case.
Also make '-t' have an effect when used with '-P' but without '-g'.

Future work: The language around '-t' perhaps needs refining, since it's
absence just lowers the priority of test packages, not prohibiting their
use (so in this particular case, package-with-only-a-test-version will
be installed, even without '-t')

libsolv.cc
@@ -656,8 +656,17 @@ SolverTasks::setTasks()
         case packagemeta::Install_action:
           if (pkg->desired)
             add(pkg->desired, taskInstall); // install/upgrade
-          else if (pkg->curr)
-            add(pkg->curr, taskInstallAny); // install
+          else
+            {
+              // install
+              //
+              // empty desired means "any version", it doesn't matter which one
+              // we use as taskInstallAny only match on the provides name
+              if (pkg->curr)
+                add(pkg->curr, taskInstallAny);
+              else if (pkg->exp)
+                add(pkg->exp, taskInstallAny);
+            }
           break;
 
         case packagemeta::Uninstall_action:

choose.cc
@@ -166,7 +166,6 @@ ChooserPage::initialUpdateState()
   if (ForceCurrentOption)
     {
       update_mode_id = IDC_CHOOSE_SYNC;
-      changeTrust(update_mode_id, AllowTestOption, true);
     }
   else if (hasManualSelections && !UpgradeAlsoOption)
     {
@@ -177,8 +176,8 @@ ChooserPage::initialUpdateState()
   else
     {
       update_mode_id = IDC_CHOOSE_BEST;
-      changeTrust (update_mode_id, AllowTestOption, true);
     }
+  changeTrust(update_mode_id, AllowTestOption, true);
 
   static int ta[] = { IDC_CHOOSE_KEEP, IDC_CHOOSE_BEST, IDC_CHOOSE_SYNC, 0 };
   rbset (GetHWND (), ta, update_mode_id);

commit 549ab3ecfde551f8e718d833b17b57c2a4f8ca0b

Fix reinstall via command line
Since b0bd16950669, we're unconditionally generating an initial solver
solution via changeTrust(), even if manual package selections are made
on the command line.

This will discard reinstalls, as they aren't done via the solver.

Arrange to retain them by using augmentTask() to add them back to the
solution, then teach trans2db() to convert reinstalls back into
packagedb actions.

Fixes: b0bd16950669 ("Fix '-t -P package-with-only-a-test-version'")

libsolv.cc
@@ -758,7 +758,11 @@ SolverSolution::trans2db() const
         case SolverTransaction::transInstall:
           if (pv.Type() == package_binary)
             {
-              pkg->set_action(packagemeta::Install_action, pv);
+              if (pkg->get_action() == packagemeta::Uninstall_action)
+                pkg->set_action(packagemeta::Reinstall_action, pv);
+              else
+                pkg->set_action(packagemeta::Install_action, pv);
+
               pkg->default_version = pv;
             }
           else // source package

package_db.cc
@@ -631,6 +631,7 @@ void
 packagedb::defaultTrust (SolverTasks &q, SolverSolution::updateMode mode, bool test)
 {
   solution.update(q, mode, test);
+  solution.augmentTasks(q);
 
   // reflect that task list into packagedb
   solution.trans2db();